d0f26783d29ab94fb18d1d2db2d34acadb4c9268
DavidTK1198 <70339682+DavidTK1198@users.noreply.github.com>
2020-10-29 17:05:00 -0600

    TEST2

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    print("Monas Chinas")
+    print("PEPITO RULES")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
