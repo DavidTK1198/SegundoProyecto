dccb44ba395d9951e6162b87db0ef7511b36eba6
DanielMadrigalVasquez <dan1998ctpu@gmail.com>
2020-10-29 21:27:27 -0600

    hola que hace

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    print("PEPITO RULES")
+    print("monas chinas encueradas")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
